b9f4d82d6a55ee35ad20eaad5bf2ccad15687a73
IhorPyvovarov <ihor.pyvovarov@completecase.com>
2022-06-16 20:56:18 +0300

    fix routers

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import './App.css'
 const AppMain = () => {
     return (
         <div className="main-links-wrap d-flex flex-column align-items-center mt-5">
-            <Link to="/sandbox/">Sandbox</Link>
-            <Link to="/todo-classes/">To do App</Link>
-            <Link to="/movies/">Movies App</Link>
+            <Link to="sandbox/">Sandbox</Link>
+            <Link to="todo-classes/">To do App</Link>
+            <Link to="movies/">Movies App</Link>
         </div>
     )
 }
@@ -19,13 +19,13 @@ const App = () => {
     return (
         <BrowserRouter>
             <div className="bg-success text-center py-1">
-                <Link to="/" className="text-white">Back to all applications links</Link>
+                <Link to="react-study-practice/" className="text-white">Back to all applications links</Link>
             </div>
             <Routes>
-                <Route path="/" element={<AppMain />}></Route>
-                <Route path="/todo-classes/" element={<AppTodoClasses />}></Route>
-                <Route path="/movies/" element={<AppMovies />}></Route>
-                <Route path="/sandbox/" element={<AppNep />}></Route>
+                <Route path="react-study-practice/" element={<AppMain />}></Route>
+                <Route path="react-study-practice/todo-classes/" element={<AppTodoClasses />}></Route>
+                <Route path="react-study-practice/movies/" element={<AppMovies />}></Route>
+                <Route path="react-study-practice/sandbox/" element={<AppNep />}></Route>
             </Routes>
         </BrowserRouter>
     );
